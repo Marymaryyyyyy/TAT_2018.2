5c973ecba9b38eef04ed26680a0bb383584d5687
Mariya Hruzinava <mary.gruzinova@gmail.com>
2018-10-24 23:10:00 +0300

    Add files.

---
+++ Task_DEV_3/Task_DEV_3/EntryPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_DEV_3
+{
+    /// <summary>
+    /// This is the entry point of the program.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int decimalNumber
+;
+            int convertedToValue;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error! Incorrect format of the input data!");
+                return ;
+            }
+            
+            try
+            {
+                decimalNumber = Int32.Parse(args[0]);
+                convertedToValue = Int32.Parse(args[1]);
+                if (convertedToValue > 1 && convertedToValue < 21)
+                {
+                    Convertor converter = new Convertor();
+                    Console.WriteLine("Output: {0}", converter.ConvertTo(decimalNumber, convertedToValue));
+                }
+                else
+                {
+                    Console.WriteLine("Error! Incorrect format of the input data!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
