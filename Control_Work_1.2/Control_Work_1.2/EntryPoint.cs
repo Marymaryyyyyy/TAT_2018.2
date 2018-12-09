d7082eb754f59508864e957fb76189a24c5670bf
Mariya Hruzinava <mary.gruzinova@gmail.com>
2018-12-09 14:34:47 +0300

    Created a programm

---
+++ Control_Work_1.2/Control_Work_1.2/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Control_Work_1._2
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LinkSeeker linkSeeker = new LinkSeeker();
+                if (args.Length != 1)
+                {
+                    throw new Exception("Incorrect number of arguments"); 
+                }
+
+                if (args[0].Length == 0)
+                {
+                    throw new Exception("Your string is empty"); 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
