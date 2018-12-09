7bddc862014762ddb549fea5c6e362ff5f05b9a2
Mariya Hruzinava <mary.gruzinova@gmail.com>
2018-12-09 13:22:28 +0300

    Created a programm, that displays a pair of consecutive letters, as often as possible in the line

---
+++ Control_Work_1.1/Control_Work_1.1/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Control_Work_1._1
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] symbolsString)
+        {
+            try
+            {
+                EqualPairedSymbolsCounter counter = new EqualPairedSymbolsCounter(symbolsString[0]);
+                if (symbolsString.Length != 1)
+                {
+                    throw new Exception("Incorrect number of arguments"); 
+                }
+
+                if (symbolsString[0].Length == 0)
+                {
+                    throw new Exception("Your string is empty");
+                }
+                Console.WriteLine(counter.FindSimilarSimbolsSequance());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
